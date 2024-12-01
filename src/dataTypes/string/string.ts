4273ed9ce3639dc4444d4856ba4935c74e1cd287
Suhas Bhattu <suhasbhattu@gmail.com>
2024-12-01 13:38:08 +0530

    changes

--- src/dataTypes/string/string.ts
+++ src/dataTypes/string/string.ts
@@ -64,7 +64,7 @@ export const capitalize = (string: string): string => {
   if (string.length === 0) {
     return string;
   }
-  let firstChar = string[0];
+  const firstChar = string[0];
   return firstChar.toUpperCase() + string.slice(1).toLowerCase();
 };
 
